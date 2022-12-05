b6e78e2e8b1d197ac104d54923ae95f145234475
Samer A <thedev.samer@gmail.com>
2022-12-05 10:04:05 +0200

    modified the calRemainingTime algorithm

--- models/assignment.model.js
+++ models/assignment.model.js
@@ -63,35 +63,38 @@ assignmentSchema.statics.getRemainingTime = function (endDate) {
       return "time ended";
     }
 
+    const times = [];
+
     // Calc days difference
     const diffInDays = Math.floor(diffInMs / dayInMs);
     diffInMs = diffInMs - diffInDays * dayInMs;
-
-    if (diffInDays > 0) {
-      return `${diffInDays} days`;
-    }
+    times.push({ type: "days", diff: diffInDays });
 
     // Calc hours difference
     const diffInHours = Math.floor(diffInMs / hourInMs);
     diffInMs = diffInMs - diffInHours * hourInMs;
-
-    if (diffInHours > 0) {
-      return `${diffInHours} hours`;
-    }
+    times.push({ type: "hours", diff: diffInHours });
 
     // Calc minutes difference
     const diffInMins = Math.floor(diffInMs / minInMs);
     diffInMs = diffInMs - diffInMins * minInMs;
-
-    if (diffInMins > 0) {
-      return `${diffInMins} minutes`;
-    }
+    times.push({ type: "mins", diff: diffInMins });
 
     // Calc minutes difference
     const diffInSecs = Math.floor(diffInMs / secInMs);
     diffInMs = diffInMs - diffInSecs * secInMs;
+    times.push({ type: "secs", diff: diffInSecs });
 
-    return `${diffInSecs} seconds`;
+    const result = [];
+    for (let time of times) {
+      if (result.length === 2) break;
+
+      if (time.diff > 0) {
+        result.push(`${time.diff} ${time.type}`);
+      }
+    }
+
+    return result.join(" ");
   } catch (err) {
     return "time ended";
   }
@@ -111,35 +114,38 @@ assignmentSchema.methods.getRemainingTime = function () {
       return "time ended";
     }
 
+    const times = [];
+
     // Calc days difference
     const diffInDays = Math.floor(diffInMs / dayInMs);
     diffInMs = diffInMs - diffInDays * dayInMs;
-
-    if (diffInDays > 0) {
-      return `${diffInDays} days`;
-    }
+    times.push({ type: "days", diff: diffInDays });
 
     // Calc hours difference
     const diffInHours = Math.floor(diffInMs / hourInMs);
     diffInMs = diffInMs - diffInHours * hourInMs;
-
-    if (diffInHours > 0) {
-      return `${diffInHours} hours`;
-    }
+    times.push({ type: "hours", diff: diffInHours });
 
     // Calc minutes difference
     const diffInMins = Math.floor(diffInMs / minInMs);
     diffInMs = diffInMs - diffInMins * minInMs;
-
-    if (diffInMins > 0) {
-      return `${diffInMins} minutes`;
-    }
+    times.push({ type: "mins", diff: diffInMins });
 
     // Calc minutes difference
     const diffInSecs = Math.floor(diffInMs / secInMs);
     diffInMs = diffInMs - diffInSecs * secInMs;
+    times.push({ type: "secs", diff: diffInSecs });
 
-    return `${diffInSecs} seconds`;
+    const result = [];
+    for (let time of times) {
+      if (result.length === 2) break;
+
+      if (time.diff > 0) {
+        result.push(`${time.diff} ${time.type}`);
+      }
+    }
+
+    return result.join(" ");
   } catch (err) {
     return "time ended";
   }
