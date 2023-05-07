ace519490bd08504e2db8e91f93352d983fd745c
cleversamerr <thedev.samer@gmail.com>
2023-05-07 11:32:29 +0300

    fixed a bug in search room members

--- services/room/rooms.service.js
+++ services/room/rooms.service.js
@@ -1094,34 +1094,17 @@ module.exports.unpinRoom = async (roomId) => {
 
 module.exports.searchRoomMembers = async (user, roomId, searchText) => {
   try {
-    // Check if room exists
-    const room = await Room.findById(roomId);
-    if (!room) {
-      const statusCode = httpStatus.NOT_FOUND;
-      const message = errors.rooms.notFound;
-      throw new ApiError(statusCode, message);
-    }
-
-    // Check if user is the author of the room
-    if (room.author.toString() !== user._id.toString()) {
-      const statusCode = httpStatus.FORBIDDEN;
-      const message = errors.rooms.unauthorized;
-      throw new ApiError(statusCode, message);
-    }
-
     if (!searchText) {
       return [];
     }
 
-    const members = await User.find({
-      _id: { $in: room.members },
-      $or: [
-        { firstname: { $regex: searchText, $options: "i" } },
-        { lastname: { $regex: searchText, $options: "i" } },
-      ],
-    });
+    const members = await this.getRoomMembers(user, roomId);
 
-    return members;
+    return members.filter(
+      (member) =>
+        member.firstName.includes(searchText) ||
+        member.lastName.includes(searchText)
+    );
   } catch (err) {
     throw err;
   }
